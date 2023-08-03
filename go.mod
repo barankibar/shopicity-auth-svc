ac5be7b795aa11940850c95d907f90520f1e8782
barankibar <barankibarr@outlook.com>
2023-08-03 23:28:59 +0300

    jwt generation and validation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/barankibar/shopicity-auth-svc
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/spf13/viper v1.16.0
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.30.0
--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -1,1 +1,64 @@
 package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/barankibar/shopicity-auth-svc/pkg/models"
+	"github.com/golang-jwt/jwt"
+)
+
+type JwtWrapper struct {
+	SecretKey       string
+	Issuer          string
+	ExpirationHours int64
+}
+
+type JwtClaims struct {
+	jwt.StandardClaims
+	Id    int64
+	Email string
+}
+
+func (w *JwtWrapper) GenerateToken(user models.User) (signedToken string, err error) {
+	claims := &JwtClaims{
+		Id:    user.Id,
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(w.ExpirationHours)).Unix(),
+			Issuer:    w.Issuer,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err = token.SignedString([]byte(w.SecretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func (w *JwtWrapper) ValidateToken(signedToken string) (claims *JwtClaims, err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JwtClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(w.SecretKey), nil
+		},
+	)
+	if err != nil {
+		return
+	}
+
+	claims, ok := token.Claims.(*JwtClaims)
+	if !ok {
+		return nil, errors.New("COULDN'T PARSE CLAIMS")
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		return nil, errors.New("JWT is expired")
+	}
+
+	return claims, nil
+}
